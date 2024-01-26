5cced457d34b3ab8a5bbcef6e249de46838b11a8
VladyslavKshenin <legendary.1bk@gmail.com>
2024-01-26 14:17:05 +0200

    file update

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,44 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {}
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+class House {
+  door: boolean = false;
+  key: Key;
+  tenants: Person[] = [];
+  constructor(key: Key) {
+    this.key = key;
+  }
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  openDoor(key: Key): void {
+    this.key = key;
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key(Math.random());
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +46,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
